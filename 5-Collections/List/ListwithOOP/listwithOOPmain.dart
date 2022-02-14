f110e85a7f32c3785029c65bfdcbd77fc245eaad
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2022-02-14 17:24:23 +0300

    Update listwithOOPmain.dart

--- 5-Collections/List/ListwithOOP/listwithOOPmain.dart
+++ 5-Collections/List/ListwithOOP/listwithOOPmain.dart
@@ -2,13 +2,29 @@ import 'package:collections/List/ListwithOOP/classStudents.dart';
 
 void main(){
   var student1 = Students(100,'Ali','Business');
-  var student2 = Students(123, 'Ahmet', 'Economics');
+  var student2 = Students(123, 'Veli', 'Economics');
   
   var students = <Students>[];
 
   students.add(student1);
   students.add(student2);
 
+  for(var m in students){
+    print('**********************');
+    print('Student No : ${m.number}');
+    print('Student name : ${m.name}');
+    print('Student Class : ${m.className}');
+  }
+
+  // research -> Comparable and compareTo ERROR
+
+  // List Filter
+  Iterable<Students> filteredIterable = students.where((i){
+    return i.number>120;
+  });
+
+  // converting iterable to list
+  students = filteredIterable.toList();
   for(var m in students){
     print('**********************');
     print('Student No : ${m.number}');
