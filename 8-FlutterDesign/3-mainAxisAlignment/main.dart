f5b9a49322e217cd8e04f3c5366a0f4aeb4df43a
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2022-02-17 15:24:59 +0300

    Add files via upload

---
+++ 8-FlutterDesign/3-mainAxisAlignment/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Title'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(width: 80,height: 80,color: Colors.yellow,),
+          Container(width: 80,height: 80,color: Colors.blue,),
+          Container(width: 80,height: 80,color: Colors.red,),
+        ],
+      ),
+
+    );
+  }
+}
