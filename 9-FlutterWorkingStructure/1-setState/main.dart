c439874384cf731f2e3683040c1abcc5d50ddf45
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2022-02-21 01:40:18 +0300

    Add files via upload

---
+++ 9-FlutterWorkingStructure/1-setState/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'App Structure'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            ElevatedButton(
+              onPressed:(){
+                setState(() {
+                  counter = counter + 1 ;
+
+                });
+              },
+              child:Text(
+                'Click'
+            ),
+            ),
+            Text('Result : $counter'),
+          ],
+        ),
+      ),
+    );
+  }
+}
