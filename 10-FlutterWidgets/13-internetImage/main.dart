bd4584dca51f029a53a2dc73d5622e759efb0b3d
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2022-02-23 15:30:51 +0300

    Add files via upload

---
+++ 10-FlutterWidgets/13-internetImage/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String imageName = 'django.png' ;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Internet Image'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network('http://kasimadalan.pe.hu/filmler/resimler/${imageName}'),
+            ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    imageName = 'inception.png';
+                  });
+
+            },
+                child: Text('Inception'),
+            ),
+
+            ElevatedButton(
+              onPressed: (){
+                setState(() {
+                  imageName = 'django.png';
+                });
+              },
+              child: Text('Django'),
+            ),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
