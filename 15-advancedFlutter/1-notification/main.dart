fdf2ce5bd0288cea4eef779dedee68f0912abd72
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2022-03-02 00:54:41 +0300

    Update main.dart

--- 15-advancedFlutter/1-notification/main.dart
+++ 15-advancedFlutter/1-notification/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
 
 void main() {
   runApp(const MyApp());
