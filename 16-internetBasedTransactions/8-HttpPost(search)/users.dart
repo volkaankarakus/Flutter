e923a2ae0355d7d4fc63865e24ede9ddfc739e51
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2022-03-03 18:59:34 +0300

    Add files via upload

---
+++ 16-internetBasedTransactions/8-HttpPost(search)/users.dart
@@ -0,0 +1,15 @@
+class Users{
+  String userID;
+  String userName;
+  String userPhone;
+
+  Users(this.userID, this.userName, this.userPhone);
+
+  factory Users.fromJson(Map<String,dynamic> json){
+    return Users(
+      json["kisi_id"] as String,
+      json["kisi_ad"] as String,
+      json["kisi_tel"] as String,
+    );
+  }
+}
